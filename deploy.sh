caed452583df7907f3707e231a67db4e7e46be8d
Debonil Ghosh <debonil.ghosh@gmail.com>
2021-05-29 23:57:42 +0530

    deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 git checkout gh-pages && 
-git rebase master &&
+git merge master &&
 ng build --prod --output-path docs --base-href //socket-io-ng-client/  &&
 cp -a docs/index.html docs/404.html  &&
+# find . ! -name 'docs' -type d -exec rm -rf {} +
 git add ./  &&
 git commit -m "Released $(date '+%Y-%m-%d  %H:%M:%S')"  &&
-git push  -f &&
+git push &&
 git checkout master
