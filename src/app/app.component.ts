8f2ecd1fcfbd5f663670094c00b2a55ec71afca7
Debonil Ghosh <debonil.ghosh@gmail.com>
2021-05-30 02:10:16 +0530

    html changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'socket-io-ng-client';
+  title = 'socket-io-ng-client v 1.0.0';
 }
